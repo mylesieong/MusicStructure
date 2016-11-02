5f3321bfffc0e7d28b97440550f02b627b44900d
Myles Ieong <Myles Ieong>
2016-11-02 22:21:45 +0800

    Make the basic of activity_main.xml and Build empty favoriteActivity componenet with button connection

--- app/src/main/java/com/myles/udacity/musicstructure/MainActivity.java
+++ app/src/main/java/com/myles/udacity/musicstructure/MainActivity.java
@@ -1,7 +1,11 @@
 package com.myles.udacity.musicstructure;
 
-import android.support.v7.app.AppCompatActivity;
+import android.content.Intent;
 import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.view.View;
+import android.view.View.OnClickListener;
+import android.widget.Button;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -9,5 +13,17 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        Button mFavoriteButton = (Button)findViewById(R.id.button_favorite);
+        mFavoriteButton.setOnClickListener(new OnClickListener(){
+            @Override
+            public void onClick(View view) {
+                // Create a new intent to open the {@link FamilyActivity}
+                Intent favoriteIntent = new Intent(MainActivity.this, FavoriteActivity.class);
+
+                // Start the new activity
+                startActivity(favoriteIntent);
+            }
+        });
     }
 }

02a203eb8debfd5cc3e6590cf5f590143f4bd2a8
Myles Ieong <Myles Ieong>
2016-11-03 22:10:26 +0800

    Add button to now playing in download activity and put explainatory text in download

--- app/src/main/java/com/myles/udacity/musicstructure/DownloadActivity.java
+++ app/src/main/java/com/myles/udacity/musicstructure/DownloadActivity.java
@@ -1,5 +1,6 @@
 package com.myles.udacity.musicstructure;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.support.design.widget.FloatingActionButton;
 import android.support.design.widget.Snackbar;
@@ -13,6 +14,13 @@ public class DownloadActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_download);
+
+        findViewById(R.id.button_nowplaying).setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                startActivity(new Intent(DownloadActivity.this, NowPlayingActivity.class));
+            }
+        });
 
     }
 
